416e1a4889c0f40fad4f323d342736240201fbd7
Mohit Tomer <mohit.tomer@verizonconnect.com>
2024-04-21 23:28:17 +0530

    Initial Commit

---
+++ CombineBasics/CombineBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineBasicsApp.swift
+//  CombineBasics
+//
+//  Created by Mohit Tomer on 21/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CombineBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineBasics/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CombineBasics
+//
+//  Created by Mohit Tomer on 21/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
